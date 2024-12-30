7137ae0ca70a4cc12850ecb5a4e725fc0baa3953
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-30 11:05:53 +0300

    Logo Ticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/sections/header";
 import { Hero } from "@/sections/hero";
+import { LogoTicker } from "@/sections/logo-ticker";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
--- src/sections/logo-ticker.tsx
+++ src/sections/logo-ticker.tsx
@@ -1,3 +1,56 @@
+import Image from "next/image";
+
+import acmeLogo from "@/assets/logo-acme.png";
+import apexLogo from "@/assets/logo-apex.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import echoLogo from "@/assets/logo-echo.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+
+const logos = [
+  {
+    name: "Acme",
+    image: acmeLogo,
+  },
+  {
+    name: "Quantum",
+    image: quantumLogo,
+  },
+  {
+    name: "Echo Valley",
+    image: echoLogo,
+  },
+  {
+    name: "Celestial",
+    image: celestialLogo,
+  },
+  {
+    name: "Pulse",
+    image: pulseLogo,
+  },
+  {
+    name: "Apex",
+    image: apexLogo,
+  },
+];
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <section className="bg-white py-8 md:py-12">
+      <div className="container lg:px-10">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex flex-none gap-14">
+            {logos.map(({ name, image }) => (
+              <Image
+                key={name}
+                src={image}
+                alt={name}
+                className="logo-ticker-image"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
