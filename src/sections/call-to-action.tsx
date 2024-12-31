70afb38414336267959d792b8cf1c35c290bae39
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-31 13:20:56 +0300

    Call to Action Animation

--- src/sections/call-to-action.tsx
+++ src/sections/call-to-action.tsx
@@ -1,12 +1,27 @@
-import Image from "next/image";
+"use client";
+
+import { useRef } from "react";
+
+import { motion, useScroll, useTransform } from "framer-motion";
 
 import ArrowRight from "@/assets/arrow-right.svg";
 import springImage from "@/assets/spring.png";
 import starImage from "@/assets/star.png";
 
 export const CallToAction = () => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -300]);
+
   return (
-    <section className="overflow-x-clip bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] pb-24 lg:pt-24">
+    <section
+      ref={sectionRef}
+      className="overflow-x-clip bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] pb-24 lg:pt-24"
+    >
       <div className="container">
         <div className="section-heading-container relative text-center">
           <h2 className="section-title">Sign up for free today</h2>
@@ -20,15 +35,21 @@ export const CallToAction = () => {
               <span>Learn More</span> <ArrowRight className="size-5" />
             </button>
           </div>
-          <Image
-            src={springImage}
+          <motion.img
+            style={{
+              translateY,
+            }}
+            src={springImage.src}
             alt="spring"
             height={363}
             width={363}
             className="absolute -right-[331px] -top-[19px] hidden md:block"
           />
-          <Image
-            src={starImage}
+          <motion.img
+            style={{
+              translateY,
+            }}
+            src={starImage.src}
             alt="star"
             height={360}
             width={360}
